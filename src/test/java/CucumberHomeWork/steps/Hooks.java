2c28804330c53c1fdb7f5987ba9962a7e456c9f2
IgorTkachenko <igortkachenko987@gmail.com>
2024-07-24 21:59:43 +0200

    111

--- src/test/java/CucumberHomeWork/steps/Hooks.java
+++ src/test/java/CucumberHomeWork/steps/Hooks.java
@@ -7,28 +7,32 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 import static CucumberHomeWork.context.TestContext.*;
 
 public class Hooks {
     @Before
-    public void beforeMethod(Scenario scenario){
+    public void beforeMethod(Scenario scenario) {
         getDriver().get(ConfigurationReader.get("base_url"));
-        TestContext.scenario=scenario;
+        TestContext.scenario = scenario;
     }
 
     @After
-    public void afterMethod(){
-        if (scenario.isFailed()){
-            TakesScreenshot ts = (TakesScreenshot) getDriver();
+    public void afterMethod(Scenario scenario) {
+        if (scenario.isFailed()) {
+            WebDriver driver = getDriver();
+            if (getDriver() != null) {
+                TakesScreenshot ts = (TakesScreenshot) getDriver();
 
-            byte[] src = ts.getScreenshotAs(OutputType.BYTES);
-            scenario.attach(src, "image/png", "screenshot");
+                byte[] src = ts.getScreenshotAs(OutputType.BYTES);
+                scenario.attach(src, "image/png", "screenshot");
+            }
         }
-       if(getDriver() !=null){
-           closeDriver();
-       }
 
+        closeDriver();
     }
+
+}
 
 }
