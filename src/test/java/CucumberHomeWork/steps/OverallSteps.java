d45a4271f14c23741328325e1072ad17798eb000
IgorTkachenko <igortkachenko987@gmail.com>
2024-08-09 18:46:57 +0200

    FINAL

--- src/test/java/CucumberHomeWork/steps/OverallSteps.java
+++ src/test/java/CucumberHomeWork/steps/OverallSteps.java
@@ -9,15 +9,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static CucumberHomeWork.context.TestContext.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OverallSteps {
     MainPage mainPage = new MainPage();
 
     @Given("The user is on the main page")
     public void user_is_on_the_main_page() {
-        if (mainPage.categoriesButton.isDisplayed()) {
-            scenario.log("Hallo");
-        }
+        assertTrue(mainPage.categoriesButton.isDisplayed());
+        scenario.log("Hallo");
     }
 
     @Then("The user will see the error message {string}")
